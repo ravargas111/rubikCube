121bdf67a134ff2c3dbe42a271f8fb3e70f5cb43
Rodrigo Vargas <42876568+ravargas111@users.noreply.github.com>
2018-09-20 21:20:12 -0600

    nuevas views

---
+++ RubikCube/src/rubikcube/view/InicioController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubikcube.view;
+
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author robri
+ */
+public class InicioController implements Initializable {
+
+    @FXML
+    private JFXTextField txtNombre;
+    @FXML
+    private ImageView imgCubo;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void selModo(ActionEvent event) {
+    }
+    
+}
